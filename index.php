e5b80482e9c801e0f9ae3daec9912eacf5f054d2
CJ <crebeta@winthrop.org>
2015-04-03 14:59:49 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+	    <title>Winthrop Medical Practice Websites</title>
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+	    <!-- Bootstrap Stylesheet -->
+	    <link href="portfolio/css/bootstrap.min.css"rel="stylesheet">
+	    <!-- Style -->
+	    <link href="portfolio/css/style.css"rel="stylesheet">
+	</head>
+
+	<body>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1 class="text-center">Winthrop Medical Practice Websites</h1>
+				</div>
+			</div>
+
+			<div class="sites">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="site">
+							<div class="overlay"></div><iframe class="frame" src="http://wuhwebsites.winthrop.org/bethpage_wordpress/" scrolling="no"></iframe>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="site">
+							<div class="overlay"></div><iframe class="frame" src="http://wuhwebsites.winthrop.org/garden_city_primary_wordpress/" scrolling="no"></iframe>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="site">
+							<div class="overlay"></div><iframe class="frame" src="http://wuhwebsites.winthrop.org/shoreheart_wordpress/" scrolling="no"></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="site">
+							<div class="overlay"></div><iframe class="frame" src="http://wuhwebsites.winthrop.org/southshorecardiovascular_wordpress/" scrolling="no"></iframe>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="site">
+							<div class="overlay"></div><iframe class="frame" src="http://wuhwebsites.winthrop.org/advanced_neuro_wordpress/" scrolling="no"></iframe>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="site">
+							<div class="overlay"></div><iframe class="frame" src="http://wuhwebsites.winthrop.org/eastend_cardio_wordpress/" scrolling="no"></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="site">
+							<div class="overlay"></div><iframe class="frame" src="http://wuhwebsites.winthrop.org/cipolla_wordpress/" scrolling="no"></iframe>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="site">
+							<div class="overlay"></div><iframe class="frame" src="http://wuhwebsites.winthrop.org/advanced_ortho_wordpress/" scrolling="no"></iframe>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</body>
+
+	<!-- Bootstrap -->
+	
+</html>
